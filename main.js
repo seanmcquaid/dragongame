b584dff66c26ff07115419b76749fe5fe3761367
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-08 15:58:39 -0500

    starting to work on implementing power system for each character

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var dicePic2 = document.getElementById("dice2");
 var fightButton = document.getElementById("fight");
 var fleeButton = document.getElementById("flee");
 
+var heroHealth = 20;
+var dragonHealth = 30;
+
 var instructions = document.getElementById("instructions");
 instructions.innerHTML = `
     <p>If you decide to fight, you must roll a 9 or higher between the two dice to slay the dragon or you will be burnt to a crisp!</p>
@@ -16,15 +19,24 @@ instructions.innerHTML = `
 function fight(){
     var randomNumber1 = Math.ceil(Math.random(2,6) * 6);
     var randomNumber2 = Math.ceil(Math.random(2,6) * 6);
-    var score = randomNumber1 + randomNumber2
+    var score = randomNumber1 + randomNumber2;
+    var heroPower = score;
+    var dragonPower = 5;
+    var newHeroHealth = heroHealth - dragonPower;
+    var newDragonHealth = dragonHealth - heroPower;
+    console.log(newHeroHealth);
+    console.log(newDragonHealth);
     dicePic1.innerHTML = `<img src="images/d${randomNumber1}.gif">`;
     dicePic2.innerHTML = `<img src="images/d${randomNumber2}.gif">`;
-    if (score >= 9) {
+    if (dragonHealth == 0) {
         dragon.innerHTML = '<img src="images/slainDragon.jpg">';
-        instructions.innerHTML = "You've won and the dragon is now your throne!!!"
-    } else {
+        instructions.innerHTML = "You've won and the dragon is now your throne!!!";
+    } else if (heroHealth == 0){
         dragon.innerHTML = '<img src="images/fireDragon.jpg">';
-        instructions.innerHTML = "You've lost and you are now burnt to a crisp!!!"
+        instructions.innerHTML = "You've lost and you are now burnt to a crisp!!!";
+    } else {
+        dragon.innerHTML = '<img src="images/fight.jpg">';
+        instructions.innerHTML = "Keep fighting!!";
     }
 };
 
@@ -35,3 +47,6 @@ function flee(){
 
 fightButton.addEventListener("click",fight);
 fleeButton.addEventListener("click",flee);
+
+
+// while health is greater than 0, run game
